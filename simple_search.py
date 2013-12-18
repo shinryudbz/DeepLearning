3f9554234607ee19aa5a55876bdb1f2360099374
Salik Syed <salik@parastructure.com>
2013-12-17 21:40:42 -0800

    pseudo-code of algorithm

--- simple_search.py
+++ simple_search.py
@@ -6,5 +6,4 @@ if __name__ == '__main__':
 	print "Loading Model"
 	model =  Word2Vec.load_word2vec_format('model/wgt.out', binary=False)
 	print "Done"
-	print model.most_similar(positive=['votes_cool_70.0_percentile'], negative=['votes_funny_60.0_percentile'], topn=100)
-	print model.most_similar(positive=['votes_cool_70.0_percentile', 'votes_funny_60.0_percentile'], topn=100)
+	print model.most_similar(positive=['review_count_90.0_percentile'], topn=100)
