da9c851ba7310ce14bf2bd8f3e9e65648f3b6740
Salik Syed <salik@parastructure.com>
2013-12-23 12:40:17 -0800

    adding search

--- simple_search.py
+++ simple_search.py
@@ -2,25 +2,31 @@ from gensim.models.word2vec import *
 import hungarian
 
 
-def convert_to_word_array(doc, fieldsToCompare):
+
+
+
+def convert_json_to_sentence(doc, fieldsToCompare):
 	#TODO
 	return ["review_count_90.0_percentile", "votes_cool_80.0_percentile", "love"];
 
-def compare_docs(docA, docB, fieldsToCompare, model):
-	wordsA = ["review_count_90.0_percentile", "food", "love"] #convert_to_word_array(docA, fieldsToCompare)
-	wordsB = ["review_count_90.0_percentile", "phoenix", "food"] #convert_to_word_array(docA, fieldsToCompare)
+# Document comparison algorithm
+# Given  documents A,B and |A|<|B|
+# assign an edge between all points in A to all points in B with weight = 1 / similarity(p1,p2)
+# run hungarian algorithm to find maximal matching (this yields a subset of A's points)
+# return sum of weights as score
 
-	
-	n = max(len(wordsA), len(wordsB))
+def compare_docs(sentenceA, sentenceB, model):
 
-	if(len(wordsA) < n):
-		shorter = wordsA
-		longer = wordsB
+	n = max(len(sentenceA), len(sentenceB))
+
+	if(len(sentenceA) < n):
+		shorter = sentenceA
+		longer = sentenceB
 	else:
-		shorter = wordsB
-		longer = wordsA
+		shorter = sentenceB
+		longer = sentenceA
 
-	# compute distance matrix from A=>B
+	# compute distance matrix from A => B
 	mat = []
 	for i in xrange(0, n):
 		row = [0 for m in xrange(0,n)];
@@ -32,14 +38,24 @@ def compare_docs(docA, docB, fieldsToCompare, model):
 		mat.append(row)
 
 	# run hungarian algorithm on cost matrix
-	scores = hungarian.lap(mat)[1]
-
+	matches = hungarian.lap(mat)[0]
 	# sum over minimal distance matching
 	total = 0
 	for i in xrange(0, len(shorter)):
-		total += model.similiarity(1.0 / shorter[scores[i]], longer[i])
+		total += model.similarity(shorter[i], longer[matches[i]])
 	return total
 
+
+def run_search(doc, dataset, numResults, fieldsToCompare):
+	docA = convert_json_to_sentence(doc,fieldsToCompare);
+	# build a search queue
+	# scan through dataset
+	for docToCompare in dataset:
+		docB = convert_json_to_sentence(docToCompare, fieldsToCompare)
+		score = compare_docs(docA, docB,)
+		# push docB with score onto priority queue
+		
+		
 
 
 if __name__ == '__main__':
@@ -48,17 +64,7 @@ if __name__ == '__main__':
 	print "Done"
 	#print model.most_similar(positive=['review_count_90.0_percentile'], topn=100)
 
-	#Basic algorithm (to start):
-	# Given  doc A with |A| = i and doc B with |B| = j and i<j
-	# pick a set of i unique points in B which result in the lowest total distance
-	# (assign an edge between all points in A to all points in B with weight= 1 / distance)
-	# run hungarian algorithm to find maximal matching (this yields subset of A's points)
-	# return sum of weights as score
-	# 
-	# So function looks like
-	# Compare(docA, docB, fieldsToCompare, model)
-	# Need function which given JSON of docA, docB, and fieldsToCompare outputs
-	# list of words that we can stick into the model
-	
-	compare_docs(None, None, None, model)
+	sentenceA = ["review_count_90.0_percentile", "food", "love"];
+	sentenceB = ["review_count_90.0_percentile", "phoenix", "food", "blah", "love"]
+	compare_docs(wordsA, wordsB, model)
 
